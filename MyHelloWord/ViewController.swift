55e214e0e3909617bfafc5c08d3d001383633979
student <student@imac-16.local>
2018-11-22 14:54:42 +0800

    Initial Commit

---
+++ MyHelloWord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHelloWord
+//
+//  Created by student on 2018/11/22.
+//  Copyright © 2018年 MyHelloWord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //设置背景色
+        //view.backgroundColor = UIColor.blue
+        
+        
+        
+    }
+
+
+}
+
